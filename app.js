3a80a14cc51fa3d41ed2eda5774bb6289ec9803a
sixsixsooo <yosheevgen@gmail.com>
2025-07-31 16:19:07 +0300

    Map()
    How to change key and value

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const users = new Map([
+let users = new Map([
   [1, "Eugeniy"],
   [2, "Til"],
   [3, "Rick"],
@@ -23,3 +23,6 @@ console.log([...users]);
 console.log([...users.keys()]);
 
 console.log([...users.values()]);
+
+users = new Map([...users].map((el) => el.reverse()));
+console.log(users);
