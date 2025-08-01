99947b0c5c313b5bcd2641c77f4c86035e8822ba
sixsixsooo <yosheevgen@gmail.com>
2025-08-01 23:19:48 +0300

    Math.random()
    Lerned how to solve easy way to randoming and rounding numbers.

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 "use strict";
 
-console.log(Number("40"));
+console.log(Math.cbrt(27));
 
-console.log(Number.parseInt("20 and cats"));
+console.log(Math.sqrt(4));
 
-console.log(Number.parseInt("1000 and 100"));
+console.log(4 ** (1 / 2));
+console.log(27 ** (1 / 3));
 
-console.log(Number.parseInt("10"));
+console.log(2 ** 3);
 
-console.log(Number.isFinite(10 / 0));
-console.log(Number.isFinite(Number("87NeValidno")));
-console.log(Number.isFinite(10));
+console.log(Math.max(9, 0, -1, -100, 20, 1000, true, "10"));
+console.log(Math.min(9, 0, -1, -100, 20, 1000, true, "10"));
 
-console.log(Number.isNaN(Number("10dedw")));
-console.log(Number.isNaN(10));
+let chisla = [9, 0, -1, -100, 20, 1000, true, "10"];
+
+console.log(Math.max(chisla));
+console.log(Math.max(...chisla));
+
+console.log(Math.round(10.122));
+console.log(Math.round(10.566));
+
+console.log(Math.ceil(10.1));
+console.log(Math.floor(10.1));
+console.log(Math.trunc(10.1));
+console.log(Math.trunc(10.80009909));
+
+console.log((400.199).toFixed(1));
+console.log(Number((400.199).toFixed(1)));
+
+function randomNumber(num) {
+  num = Math.round(Math.random() * 20 + 1); // Better be write 21 cuz it's more faster and optimized
+  console.log(num);
+}
+randomNumber();
+
+function randomNumberBetter(num) {
+  num = Math.round(Math.random() * 20 + 1); // Better be write 21 cuz it's more faster and optimized
+  return num;
+}
+console.log(randomNumberBetter());
+
+function randomNumberBetterAll(min, max) {
+  const num = Math.round(Math.random() * (max - min + 1) + min);
+  return num;
+}
+console.log(randomNumberBetter(1, 20));
