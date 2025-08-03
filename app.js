15b45a53e99facbf75d8d7eec67e50a0f16f9563
sixsixsooo <yosheevgen@gmail.com>
2025-08-03 22:47:59 +0300

    Math()
    Add: remainder from division

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
 "use strict";
 
-console.log(Math.cbrt(27));
+"use strict";
 
-console.log(Math.sqrt(4));
+const arrNmbersForEven = [2, 3, 4, 5, 6, 1, 3, 2, 0];
+const arrNmbersForOdd = [2, 3, 4, 5, 6, 1, 3, 2, 0];
 
-console.log(4 ** (1 / 2));
-console.log(27 ** (1 / 3));
+const isEven = (n) => n % 2 === 0;
+const isOdd = (n) => n % 2 === 1;
 
-console.log(2 ** 3);
-
-console.log(Math.max(9, 0, -1, -100, 20, 1000, true, "10"));
-console.log(Math.min(9, 0, -1, -100, 20, 1000, true, "10"));
-
-let chisla = [9, 0, -1, -100, 20, 1000, true, "10"];
-
-console.log(Math.max(chisla));
-console.log(Math.max(...chisla));
-
-console.log(Math.round(10.122));
-console.log(Math.round(10.566));
-
-console.log(Math.ceil(10.1));
-console.log(Math.floor(10.1));
-console.log(Math.trunc(10.1));
-console.log(Math.trunc(10.80009909));
-
-console.log((400.199).toFixed(1));
-console.log(Number((400.199).toFixed(1)));
-
-function randomNumber(num) {
-  num = Math.round(Math.random() * 20 + 1); // Better be write 21 cuz it's more faster and optimized
-  console.log(num);
+function reserve(n) {
+  return n % 2 === 0;
 }
-randomNumber();
 
-function randomNumberBetter(num) {
-  num = Math.round(Math.random() * 20 + 1); // Better be write 21 cuz it's more faster and optimized
-  return num;
-}
-console.log(randomNumberBetter());
+const isEvenRes = arrNmbersForEven.filter(isEven);
+const isOddRes = arrNmbersForOdd.filter(isOdd);
 
-function randomNumberBetterAll(min, max) {
-  const num = Math.round(Math.random() * (max - min + 1) + min);
-  return num;
-}
-console.log(randomNumberBetter(1, 20));
+const resEven = isEvenRes;
+const resOdd = isOddRes;
+
+console.log(resEven);
+console.log(resOdd);
