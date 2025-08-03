53eeb2d9e22f42bd24a31a17bedc5ce1aabcbe31
sixsixsooo <yosheevgen@gmail.com>
2025-08-03 22:59:13 +0300

    Add: 1_000_000

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 "use strict";
 
-"use strict";
+const BigNumber1 = 1000000000;
+const BigNumber2 = 1_000_000_000;
 
-const arrNmbersForEven = [2, 3, 4, 5, 6, 1, 3, 2, 0];
-const arrNmbersForOdd = [2, 3, 4, 5, 6, 1, 3, 2, 0];
-
-const isEven = (n) => n % 2 === 0;
-const isOdd = (n) => n % 2 === 1;
-
-function reserve(n) {
-  return n % 2 === 0;
-}
-
-const isEvenRes = arrNmbersForEven.filter(isEven);
-const isOddRes = arrNmbersForOdd.filter(isOdd);
-
-const resEven = isEvenRes;
-const resOdd = isOddRes;
-
-console.log(resEven);
-console.log(resOdd);
+console.log(Number("100000"));
+console.log(Number("1_000_000"));
+console.log(Number.parseInt("1_000_000"));
