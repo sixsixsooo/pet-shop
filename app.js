c85f214794230693ad97a45b667157e74999f35f
sixsixsooo <yosheevgen@gmail.com>
2025-08-06 01:53:35 +0300

    Date()
    Add: basic operations whis Date()

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
 "use strict";
 
-function convert(number, inputNumber, convertNumber) {
-  const allCurencyes = [
-    { name: "RUB", mult: 1 / 60 },
-    { name: "USD", mult: 1 },
-    { name: "EUR", mult: 1.2 },
-  ];
+const date = new Date();
+console.log(date);
+console.log(new Date("06-08-2025"));
 
-  const input = allCurencyes.find((a) => a.name === inputNumber);
-  if (!input) {
-    return null;
-  }
-
-  const convert = allCurencyes.find((a) => a.name === convertNumber);
-  if (!convert) {
-    return null;
-  }
-
-  return new Intl.NumberFormat("ru-RU", {
-    style: "currency",
-    currency: convert.name,
-  }).format((number * input.mult) / convert.mult);
-}
-
-console.log(convert(1000, "RUB", "USD"));
-console.log(convert(1000, "USD", "EUR"));
-console.log(convert(1000, "RUB", "USD"));
-console.log(convert(1000, "EUR", "RUB"));
+console.log(Date.now());
