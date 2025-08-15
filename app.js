4722c2adc9d27438ec6af29604d0874d8f09d1e8
sixsixsooo <yosheevgen@gmail.com>
2025-08-15 21:56:12 +0300

    static in class

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 "use strict";
 
-const task = {
-  title: "task1",
-  date: new Date("2025/1/1"),
+class Test {
+  static a = 100;
 
-  get isOverdue() {
-    return this.date;
-  },
-
-  set isOverdue(date) {
-    if (task.date != date) {
-      this.date = new Date();
-    }
-  },
-};
-
-task.isOverdue = "2025/7/1";
-console.log(task);
-
-class Task {
-  constructor(title, dateTask) {
-    this.title = title;
-    this.date = dateTask;
+  static hello() {
+    console.log("hello");
   }
 
-  get isOverdue() {
-    return this.dateTask < new Date();
-  }
-
-  set dueDate(date) {
-    if (date < new Date()) {
-      return;
-    }
-    this._date = date;
+  static {
+    let b = 10000;
+    this.a = 10;
   }
 }
 
-const task1 = new Task("Js", new Date());
+Test.hello();
 
-console.log((task1.dueDate = new Date("2025/8/15")));
+console.log(Test.a);
