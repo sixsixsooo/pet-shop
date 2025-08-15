2feca8bdea2f9ef30c6dbb38d4ac991ce2893544
sixsixsooo <yosheevgen@gmail.com>
2025-08-16 02:28:54 +0300

    creaete user class whis change login, password and check password

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 "use strict";
 
-class Car {
-  #vin;
+class User {
+  #login;
+  #password;
 
-  #changeSpeed() {
-    console.log("Speed chenged");
+  constructor(login, password) {
+    this.#login = login;
+    this.#setPassword(password);
   }
 
-  speedNowa() {
-    this.#changeSpeed();
+  #encryptPassword(password) {
+    return password.split("").reverse().join("");
+  }
+
+  #setPassword(newPassword) {
+    this.#password = this.#encryptPassword(newPassword);
+  }
+
+  checkPassword(password) {
+    return this.#password === this.#encryptPassword(password);
+  }
+
+  changePassword(oldPassword, newPassword) {
+    if (this.checkPassword(oldPassword)) {
+      this.#setPassword(newPassword);
+      console.log("Password was changed!");
+    } else {
+      console.log("Old password incorrect!");
+    }
+  }
+
+  get login() {
+    return this.#login;
+  }
+  set login(newLogin) {
+    this.#login = newLogin;
+  }
+
+  changeLogin(newLogin) {
+    this.login = newLogin;
+    console.log(`Login change succesfull on ${this.#login}`);
   }
 }
 
-const car = new Car();
+const user1 = new User("Evgeniy", "qwertyqwerty");
 
-car.speedNowa();
+console.log(user1.checkPassword("qwertyqwerty"));
+console.log(user1.changeLogin("EvgeniyNew"));
+console.log(user1.changePassword("qwertyqwerty", "qwerty"));
